qgis/QGIS

Added managing of WMS legends (getLegendGraphic) in the addLegend of QgsComposer. Added parameters to interactively modify wms legends scale. Add WMS getLegendGraphic draw in composer and add it's size parameters in composer window.
Developed with funding from Regione Toscana-SITA

src/core/composer/qgscomposerlegend.cpp
@@ -54,6 +54,8 @@ QgsComposerLegend::QgsComposerLegend( QgsComposition* composition )
 
   mSymbolWidth = 7;
   mSymbolHeight = 4;
+  mWmsLegendWidth = 50;
+  mWmsLegendHeight = 25;
   mWrapChar = "";
   mlineSpacing = 1.5;
   adjustBoxSize();
@@ -347,13 +349,41 @@ QgsComposerLegend::Nucleon QgsComposerLegend::drawSymbolItem( QgsComposerLegendI
   }
   else if ( rasterItem )
   {
-    if ( painter )
+    // manage WMS lengendGraphic
+    // actual code recognise if it's a legend because it has an icon and it's text is empty => this is not good MV pattern implementation :(
+    QIcon symbolIcon = symbolItem->icon();
+    if ( !symbolIcon.isNull() && symbolItem->text().isEmpty() )
     {
-      painter->setBrush( rasterItem->color() );
-      painter->drawRect( QRectF( point.x(), point.y() + ( itemHeight - mSymbolHeight ) / 2, mSymbolWidth, mSymbolHeight ) );
+      // find max size
+      QList<QSize> sizes = symbolIcon.availableSizes();
+      double maxWidth = 0;
+      double maxHeight = 0;
+      foreach ( QSize size, sizes )
+      {
+        if ( maxWidth < size.width() ) maxWidth = size.width();
+        if ( maxHeight < size.height() ) maxHeight = size.height();
+      }
+      QSize maxSize(maxWidth, maxHeight);
+
+      // get and print pixmap
+      QPixmap pixmap = symbolIcon.pixmap(maxWidth, maxHeight);
+      if ( painter )
+      {
+        painter->drawPixmap( QRectF( point.x(), point.y(), mWmsLegendWidth, mWmsLegendHeight ), pixmap, QRectF( 0, 0, maxWidth, maxHeight ) );
+      }
+      symbolSize.rwidth() = mWmsLegendWidth;
+      symbolSize.rheight() = mWmsLegendHeight;
     }
-    symbolSize.rwidth() = mSymbolWidth;
-    symbolSize.rheight() = mSymbolHeight;
+    else
+    {
+      if ( painter )
+      {
+        painter->setBrush( rasterItem->color() );
+        painter->drawRect( QRectF( point.x(), point.y() + ( itemHeight - mSymbolHeight ) / 2, mSymbolWidth, mSymbolHeight ) );
+      }
+      symbolSize.rwidth() = mSymbolWidth;
+      symbolSize.rheight() = mSymbolHeight;
+    }
   }
   else //item with icon?
   {
@@ -555,6 +585,8 @@ bool QgsComposerLegend::writeXML( QDomElement& elem, QDomDocument & doc ) const
 
   composerLegendElem.setAttribute( "symbolWidth", QString::number( mSymbolWidth ) );
   composerLegendElem.setAttribute( "symbolHeight", QString::number( mSymbolHeight ) );
+  composerLegendElem.setAttribute( "wmsLegendWidth", QString::number( mWmsLegendWidth ) );
+  composerLegendElem.setAttribute( "wmsLegendHeight", QString::number( mWmsLegendHeight ) );
   composerLegendElem.setAttribute( "wrapChar", mWrapChar );
   composerLegendElem.setAttribute( "fontColor", mFontColor.name() );
 
@@ -622,6 +654,8 @@ bool QgsComposerLegend::readXML( const QDomElement& itemElem, const QDomDocument
 
   mSymbolWidth = itemElem.attribute( "symbolWidth", "7.0" ).toDouble();
   mSymbolHeight = itemElem.attribute( "symbolHeight", "14.0" ).toDouble();
+  mWmsLegendWidth = itemElem.attribute( "wmsLegendWidth", "50" ).toDouble();
+  mWmsLegendHeight = itemElem.attribute( "wmsLegendHeight", "25" ).toDouble();
 
   mWrapChar = itemElem.attribute( "wrapChar" );
 

src/core/composer/qgslegendmodel.cpp
@@ -225,37 +225,70 @@ int QgsLegendModel::addRasterLayerItems( QStandardItem* layerItem, QgsMapLayer* 
     return 2;
   }
 
-  QList< QPair< QString, QColor > > rasterItemList = rasterLayer->legendSymbologyItems();
-  QList< QPair< QString, QColor > >::const_iterator itemIt = rasterItemList.constBegin();
-  int row = 0;
-  for ( ; itemIt != rasterItemList.constEnd(); ++itemIt )
+  QgsDebugMsg( QString( "layer providertype:: %1" ).arg( rasterLayer->providerType() ) );
+  if ( rasterLayer->providerType() == "wms" )
   {
-    QgsComposerRasterSymbolItem* currentSymbolItem = new QgsComposerRasterSymbolItem( itemIt->first );
-
-    QgsComposerRasterSymbolItem* oldSymbolItem = dynamic_cast<QgsComposerRasterSymbolItem*>( layerItem->child( row, 0 ) );
-    if ( oldSymbolItem )
+    QgsComposerRasterSymbolItem* currentSymbolItem = new QgsComposerRasterSymbolItem( "" );
+    // GetLegendGraphics in case of WMS service... pixmap can return null if GetLegendGraphics 
+    // is not supported by server
+    // double currentScale = legend()->canvas()->scale();
+    // BEAWARE getLegendGraphic() COULD BE USED WITHOUT SCALE PARAMETER IF IT WAS ALREADY CALLED WITH 
+    // THIS PARAMER FROM A COMPONENT THAT CAN RECOVER CURRENT SCALE => LEGEND IN THE DESKTOP
+    // OTHERWISE IT RETURN A INVALID PIXMAP (QPixmap().isNull() == False)
+    QPixmap legendGraphic = rasterLayer->dataProvider()->getLegendGraphic();
+    if ( !legendGraphic.isNull() )
     {
-      currentSymbolItem->setUserText( oldSymbolItem->userText() );
-      currentSymbolItem->setText( currentSymbolItem->userText() );
+      QgsDebugMsg( QString( "downloaded legend with dimension Width:" )+QString::number(legendGraphic.width())+QString(" and Height:")+QString::number(legendGraphic.height()) );
+      if ( mHasTopLevelWindow )
+      {
+        //legendGraphic = legendGraphic.scaled(legendGraphic.width()*3, legendGraphic.height()*3, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+        currentSymbolItem->setIcon( QIcon( legendGraphic ) );
+      }
+    }
+    else
+    {
+      currentSymbolItem->setText(tr("No Legend Available"));
     }
 
-    if ( mHasTopLevelWindow )
-    {
-      QPixmap itemPixmap( 20, 20 );
-      itemPixmap.fill( itemIt->second );
-      currentSymbolItem->setIcon( QIcon( itemPixmap ) );
-    }
     currentSymbolItem->setLayerID( rasterLayer->id() );
-    currentSymbolItem->setColor( itemIt->second );
-    int currentRowCount = layerItem->rowCount();
-    layerItem->setChild( currentRowCount, 0, currentSymbolItem );
-    row++;
+    currentSymbolItem->setColor( QColor() );
+    layerItem->removeRows(0, layerItem->rowCount());
+    layerItem->setChild( layerItem->rowCount(), 0, currentSymbolItem );
   }
-
-  // Delete following old items (if current number of items decreased)
-  for ( int i = layerItem->rowCount() - 1; i >= row; --i )
+  else
   {
-    layerItem->removeRow( i );
+    QList< QPair< QString, QColor > > rasterItemList = rasterLayer->legendSymbologyItems();
+    QList< QPair< QString, QColor > >::const_iterator itemIt = rasterItemList.constBegin();
+    int row = 0;
+    for ( ; itemIt != rasterItemList.constEnd(); ++itemIt )
+    {
+      QgsComposerRasterSymbolItem* currentSymbolItem = new QgsComposerRasterSymbolItem( itemIt->first );
+
+      QgsComposerRasterSymbolItem* oldSymbolItem = dynamic_cast<QgsComposerRasterSymbolItem*>( layerItem->child( row, 0 ) );
+      if ( oldSymbolItem )
+      {
+        currentSymbolItem->setUserText( oldSymbolItem->userText() );
+        currentSymbolItem->setText( currentSymbolItem->userText() );
+      }
+
+      if ( mHasTopLevelWindow )
+      {
+        QPixmap itemPixmap( 20, 20 );
+        itemPixmap.fill( itemIt->second );
+        currentSymbolItem->setIcon( QIcon( itemPixmap ) );
+      }
+      currentSymbolItem->setLayerID( rasterLayer->id() );
+      currentSymbolItem->setColor( itemIt->second );
+      int currentRowCount = layerItem->rowCount();
+      layerItem->setChild( currentRowCount, 0, currentSymbolItem );
+      row++;
+    }
+
+    // Delete following old items (if current number of items decreased)
+    for ( int i = layerItem->rowCount() - 1; i >= row; --i )
+    {
+      layerItem->removeRow( i );
+    }
   }
 
   return 0;

kadas-albireo/kadas-albireo

src/core/composer/qgscomposerlegend.cpp
@@ -54,6 +54,8 @@ QgsComposerLegend::QgsComposerLegend( QgsComposition* composition )
 
   mSymbolWidth = 7;
   mSymbolHeight = 4;
+  mWmsLegendWidth = 50;
+  mWmsLegendHeight = 25;
   mWrapChar = "";
   mlineSpacing = 1.5;
   adjustBoxSize();
@@ -347,13 +349,41 @@ QgsComposerLegend::Nucleon QgsComposerLegend::drawSymbolItem( QgsComposerLegendI
   }
   else if ( rasterItem )
   {
-    if ( painter )
+    // manage WMS lengendGraphic
+    // actual code recognise if it's a legend because it has an icon and it's text is empty => this is not good MV pattern implementation :(
+    QIcon symbolIcon = symbolItem->icon();
+    if ( !symbolIcon.isNull() && symbolItem->text().isEmpty() )
     {
-      painter->setBrush( rasterItem->color() );
-      painter->drawRect( QRectF( point.x(), point.y() + ( itemHeight - mSymbolHeight ) / 2, mSymbolWidth, mSymbolHeight ) );
+      // find max size
+      QList<QSize> sizes = symbolIcon.availableSizes();
+      double maxWidth = 0;
+      double maxHeight = 0;
+      foreach ( QSize size, sizes )
+      {
+        if ( maxWidth < size.width() ) maxWidth = size.width();
+        if ( maxHeight < size.height() ) maxHeight = size.height();
+      }
+      QSize maxSize(maxWidth, maxHeight);
+
+      // get and print legend
+      QImage legend = symbolIcon.pixmap(maxWidth, maxHeight).toImage();
+      if ( painter )
+      {
+        painter->drawImage( QRectF( point.x(), point.y(), mWmsLegendWidth, mWmsLegendHeight ), legend, QRectF( 0, 0, maxWidth, maxHeight ) );
+      }
+      symbolSize.rwidth() = mWmsLegendWidth;
+      symbolSize.rheight() = mWmsLegendHeight;
     }
-    symbolSize.rwidth() = mSymbolWidth;
-    symbolSize.rheight() = mSymbolHeight;
+    else
+    {
+      if ( painter )
+      {
+        painter->setBrush( rasterItem->color() );
+        painter->drawRect( QRectF( point.x(), point.y() + ( itemHeight - mSymbolHeight ) / 2, mSymbolWidth, mSymbolHeight ) );
+      }
+      symbolSize.rwidth() = mSymbolWidth;
+      symbolSize.rheight() = mSymbolHeight;
+    }
   }
   else //item with icon?
   {
@@ -555,6 +585,8 @@ bool QgsComposerLegend::writeXML( QDomElement& elem, QDomDocument & doc ) const
 
   composerLegendElem.setAttribute( "symbolWidth", QString::number( mSymbolWidth ) );
   composerLegendElem.setAttribute( "symbolHeight", QString::number( mSymbolHeight ) );
+  composerLegendElem.setAttribute( "wmsLegendWidth", QString::number( mWmsLegendWidth ) );
+  composerLegendElem.setAttribute( "wmsLegendHeight", QString::number( mWmsLegendHeight ) );
   composerLegendElem.setAttribute( "wrapChar", mWrapChar );
   composerLegendElem.setAttribute( "fontColor", mFontColor.name() );
 
@@ -622,6 +654,8 @@ bool QgsComposerLegend::readXML( const QDomElement& itemElem, const QDomDocument
 
   mSymbolWidth = itemElem.attribute( "symbolWidth", "7.0" ).toDouble();
   mSymbolHeight = itemElem.attribute( "symbolHeight", "14.0" ).toDouble();
+  mWmsLegendWidth = itemElem.attribute( "wmsLegendWidth", "50" ).toDouble();
+  mWmsLegendHeight = itemElem.attribute( "wmsLegendHeight", "25" ).toDouble();
 
   mWrapChar = itemElem.attribute( "wrapChar" );
 

src/core/composer/qgslegendmodel.cpp
@@ -225,37 +225,69 @@ int QgsLegendModel::addRasterLayerItems( QStandardItem* layerItem, QgsMapLayer* 
     return 2;
   }
 
-  QList< QPair< QString, QColor > > rasterItemList = rasterLayer->legendSymbologyItems();
-  QList< QPair< QString, QColor > >::const_iterator itemIt = rasterItemList.constBegin();
-  int row = 0;
-  for ( ; itemIt != rasterItemList.constEnd(); ++itemIt )
+  QgsDebugMsg( QString( "layer providertype:: %1" ).arg( rasterLayer->providerType() ) );
+  if ( rasterLayer->providerType() == "wms" )
   {
-    QgsComposerRasterSymbolItem* currentSymbolItem = new QgsComposerRasterSymbolItem( itemIt->first );
-
-    QgsComposerRasterSymbolItem* oldSymbolItem = dynamic_cast<QgsComposerRasterSymbolItem*>( layerItem->child( row, 0 ) );
-    if ( oldSymbolItem )
+    QgsComposerRasterSymbolItem* currentSymbolItem = new QgsComposerRasterSymbolItem( "" );
+    // GetLegendGraphics in case of WMS service... image can return null if GetLegendGraphics 
+    // is not supported by server
+    // double currentScale = legend()->canvas()->scale();
+    // BEAWARE getLegendGraphic() COULD BE USED WITHOUT SCALE PARAMETER IF IT WAS ALREADY CALLED WITH 
+    // THIS PARAMER FROM A COMPONENT THAT CAN RECOVER CURRENT SCALE => LEGEND IN THE DESKTOP
+    // OTHERWISE IT RETURN A INVALID PIXMAP (QPixmap().isNull() == False)
+    QImage legendGraphic = rasterLayer->dataProvider()->getLegendGraphic();
+    if ( !legendGraphic.isNull() )
     {
-      currentSymbolItem->setUserText( oldSymbolItem->userText() );
-      currentSymbolItem->setText( currentSymbolItem->userText() );
+      QgsDebugMsg( QString( "downloaded legend with dimension Width:" )+QString::number(legendGraphic.width())+QString(" and Height:")+QString::number(legendGraphic.height()) );
+      if ( mHasTopLevelWindow )
+      {
+        currentSymbolItem->setIcon( QIcon( QPixmap::fromImage(legendGraphic) ) );
+      }
+    }
+    else
+    {
+      currentSymbolItem->setText(tr("No Legend Available"));
     }
 
-    if ( mHasTopLevelWindow )
-    {
-      QPixmap itemPixmap( 20, 20 );
-      itemPixmap.fill( itemIt->second );
-      currentSymbolItem->setIcon( QIcon( itemPixmap ) );
-    }
     currentSymbolItem->setLayerID( rasterLayer->id() );
-    currentSymbolItem->setColor( itemIt->second );
-    int currentRowCount = layerItem->rowCount();
-    layerItem->setChild( currentRowCount, 0, currentSymbolItem );
-    row++;
+    currentSymbolItem->setColor( QColor() );
+    layerItem->removeRows(0, layerItem->rowCount());
+    layerItem->setChild( layerItem->rowCount(), 0, currentSymbolItem );
   }
-
-  // Delete following old items (if current number of items decreased)
-  for ( int i = layerItem->rowCount() - 1; i >= row; --i )
+  else
   {
-    layerItem->removeRow( i );
+    QList< QPair< QString, QColor > > rasterItemList = rasterLayer->legendSymbologyItems();
+    QList< QPair< QString, QColor > >::const_iterator itemIt = rasterItemList.constBegin();
+    int row = 0;
+    for ( ; itemIt != rasterItemList.constEnd(); ++itemIt )
+    {
+      QgsComposerRasterSymbolItem* currentSymbolItem = new QgsComposerRasterSymbolItem( itemIt->first );
+
+      QgsComposerRasterSymbolItem* oldSymbolItem = dynamic_cast<QgsComposerRasterSymbolItem*>( layerItem->child( row, 0 ) );
+      if ( oldSymbolItem )
+      {
+        currentSymbolItem->setUserText( oldSymbolItem->userText() );
+        currentSymbolItem->setText( currentSymbolItem->userText() );
+      }
+
+      if ( mHasTopLevelWindow )
+      {
+        QPixmap itemPixmap( 20, 20 );
+        itemPixmap.fill( itemIt->second );
+        currentSymbolItem->setIcon( QIcon( itemPixmap ) );
+      }
+      currentSymbolItem->setLayerID( rasterLayer->id() );
+      currentSymbolItem->setColor( itemIt->second );
+      int currentRowCount = layerItem->rowCount();
+      layerItem->setChild( currentRowCount, 0, currentSymbolItem );
+      row++;
+    }
+
+    // Delete following old items (if current number of items decreased)
+    for ( int i = layerItem->rowCount() - 1; i >= row; --i )
+    {
+      layerItem->removeRow( i );
+    }
   }
 
   return 0;