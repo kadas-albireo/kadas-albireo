qgis/QGIS

[FEATURE] new edit type: date/time edit

src/app/qgisapp.cpp
@@ -265,6 +265,7 @@
 // Editor widgets
 #include "qgseditorwidgetregistry.h"
 #include "qgsrelationreferencefactory.h"
+#include "qgsdatetimeeditfactory.h"
 
 //
 // Conditional Includes
@@ -597,6 +598,7 @@ QgisApp::QgisApp( QSplashScreen *splash, bool restorePlugins, QWidget * parent, 
   QgsAttributeEditorContext context;
   context.setVectorLayerTools( vectorLayerTools() );
   editorWidgetRegistry->registerWidget( "RelationReference", new QgsRelationReferenceFactory( context, tr( "Relation Reference" ) ) );
+  editorWidgetRegistry->registerWidget( "DateTimeEdit", new QgsDateTimeEditFactory( tr( "Date/Time" ) ) );
 
 
   mInternalClipboard = new QgsClipboard; // create clipboard

kadas-albireo/kadas-albireo

src/app/qgisapp.cpp
@@ -285,6 +285,7 @@
 #include "qgswebviewwidgetfactory.h"
 #include "qgscolorwidgetfactory.h"
 #include "qgsrelationreferencefactory.h"
+#include "qgsdatetimeeditfactory.h"
 
 //
 // Conditional Includes
@@ -682,6 +683,7 @@ QgisApp::QgisApp( QSplashScreen *splash, bool restorePlugins, QWidget * parent, 
   editorWidgetRegistry->registerWidget( "WebView", new QgsWebViewWidgetFactory( tr( "Web View" ) ) );
   editorWidgetRegistry->registerWidget( "Color", new QgsColorWidgetFactory( tr( "Color" ) ) );
   editorWidgetRegistry->registerWidget( "RelationReference", new QgsRelationReferenceFactory( context, tr( "Relation Reference" ) ) );
+  editorWidgetRegistry->registerWidget( "DateTime", new QgsDateTimeEditFactory( tr( "Date/Time" ) ) );
 
   mInternalClipboard = new QgsClipboard; // create clipboard
   connect( mInternalClipboard, SIGNAL( changed() ), this, SLOT( clipboardChanged() ) );