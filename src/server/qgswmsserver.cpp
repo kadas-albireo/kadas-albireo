46f48e6516dc7a7cc11a892a984c7700c78a573c
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2015-02-17 14:49:37 +0100

    Rename feature info parameters
    Ref: 1756d93de4ee331fe1a33ed9823d842a5f768399

--- src/server/qgswmsserver.cpp
+++ src/server/qgswmsserver.cpp
@@ -3067,7 +3067,7 @@ QgsRectangle QgsWMSServer::featureInfoSearchRect( QgsVectorLayer* ml, QgsMapRend
   double mapUnitTolerance = 0.0;
   if ( ml->geometryType() == QGis::Polygon )
   {
-    QMap<QString, QString>::const_iterator tolIt = mParameters.find( "POLYGON_TOLERANCE" );
+    QMap<QString, QString>::const_iterator tolIt = mParameters.find( "FI_POLYGON_TOLERANCE" );
     if ( tolIt != mParameters.constEnd() )
     {
       mapUnitTolerance = tolIt.value().toInt() * rct.mapToPixel().mapUnitsPerPixel();
@@ -3079,7 +3079,7 @@ QgsRectangle QgsWMSServer::featureInfoSearchRect( QgsVectorLayer* ml, QgsMapRend
   }
   else if ( ml->geometryType() == QGis::Line )
   {
-    QMap<QString, QString>::const_iterator tolIt = mParameters.find( "LINE_TOLERANCE" );
+    QMap<QString, QString>::const_iterator tolIt = mParameters.find( "FI_LINE_TOLERANCE" );
     if ( tolIt != mParameters.constEnd() )
     {
       mapUnitTolerance = tolIt.value().toInt() * rct.mapToPixel().mapUnitsPerPixel();
@@ -3091,7 +3091,7 @@ QgsRectangle QgsWMSServer::featureInfoSearchRect( QgsVectorLayer* ml, QgsMapRend
   }
   else //points
   {
-    QMap<QString, QString>::const_iterator tolIt = mParameters.find( "POINT_TOLERANCE" );
+    QMap<QString, QString>::const_iterator tolIt = mParameters.find( "FI_POINT_TOLERANCE" );
     if ( tolIt != mParameters.constEnd() )
     {
       mapUnitTolerance = tolIt.value().toInt() * rct.mapToPixel().mapUnitsPerPixel();
