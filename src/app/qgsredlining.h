2659d36d14614f1c62dbdcad49fc343d72350a30
Sandro Mani <manisandro@gmail.com>
2015-10-15 10:57:09 +0200

    [Redlining] Split off map tools to a separate file
    Ref: 1897bc6752905022d638e96e4562464ecf0d8186

--- src/app/qgsredlining.h
+++ src/app/qgsredlining.h
@@ -25,12 +25,8 @@ class QComboBox;
 class QSpinBox;
 class QToolButton;
 class QgisApp;
-class QgsCurvePolygonV2;
 class QgsColorButtonV2;
 class QgsRedliningLayer;
-class QgsRubberBand;
-class QgsSelectedFeature;
-class QgsVectorLayer;
 
 class QgsRedlining : public QObject
 {
@@ -79,100 +75,6 @@ class QgsRedlining : public QObject
     void updateFeatureStyle( const QgsFeatureId& fid );
     void readProject( const QDomDocument&doc );
     void writeProject( QDomDocument&doc );
-};
-
-class QgsRedliningNewShapeMapTool : public QgsMapTool
-{
-  public:
-    QgsRedliningNewShapeMapTool( QgsMapCanvas* canvas, QgsVectorLayer* layer );
-    ~QgsRedliningNewShapeMapTool();
-    void canvasPressEvent( QMouseEvent * e ) override;
-    void canvasReleaseEvent( QMouseEvent * e ) override;
-    bool isEditTool() { return true; }
-
-  protected:
-    QgsVectorLayer* mLayer;
-    QPoint mPressPos;
-    QgsAbstractGeometryV2* mGeometry;
-    QgsRubberBand* mRubberBand;
-};
-
-class QgsRedliningPointMapTool : public QgsRedliningNewShapeMapTool
-{
-  public:
-    QgsRedliningPointMapTool( QgsMapCanvas* canvas, QgsVectorLayer* layer, const QString& shape )
-        : QgsRedliningNewShapeMapTool( canvas, layer ), mShape( shape ) {}
-    void canvasPressEvent( QMouseEvent * /*e*/ ) override {}
-    void canvasReleaseEvent( QMouseEvent * e ) override;
-  private:
-    QString mShape;
-};
-
-class QgsRedliningRectangleMapTool : public QgsRedliningNewShapeMapTool
-{
-  public:
-    QgsRedliningRectangleMapTool( QgsMapCanvas* canvas, QgsVectorLayer* layer )
-        : QgsRedliningNewShapeMapTool( canvas, layer ) {}
-    void canvasMoveEvent( QMouseEvent * e ) override;
-    void canvasReleaseEvent( QMouseEvent * e ) override;
-};
-
-class QgsRedliningCircleMapTool : public QgsRedliningNewShapeMapTool
-{
-  public:
-    QgsRedliningCircleMapTool( QgsMapCanvas* canvas, QgsVectorLayer* layer )
-        : QgsRedliningNewShapeMapTool( canvas, layer ) {}
-    void canvasMoveEvent( QMouseEvent * e ) override;
-};
-
-class QgsRedliningTextTool : public QgsMapTool
-{
-  public:
-    QgsRedliningTextTool( QgsMapCanvas* canvas, QgsVectorLayer* layer )
-        : QgsMapTool( canvas ), mLayer( layer ) {}
-    void canvasReleaseEvent( QMouseEvent * e ) override;
-    bool isEditTool() { return true; }
-  private:
-    QgsVectorLayer* mLayer;
-};
-
-class QgsRedliningEditTool : public QgsMapTool
-{
-    Q_OBJECT
-  public:
-    QgsRedliningEditTool( QgsMapCanvas* canvas, QgsVectorLayer* layer );
-    ~QgsRedliningEditTool();
-    void canvasPressEvent( QMouseEvent *e ) override;
-    void canvasReleaseEvent( QMouseEvent *e ) override;
-    void canvasMoveEvent( QMouseEvent* e );
-    void canvasDoubleClickEvent( QMouseEvent *e ) override;
-    void keyPressEvent( QKeyEvent *e ) override;
-    bool isEditTool() { return true; }
-    void deactivate() override;
-
-  public slots:
-    void onStyleChanged();
-
-  signals:
-    void featureSelected( const QgsFeatureId& fid );
-    void updateFeatureStyle( const QgsFeatureId& fid );
-
-  private:
-    QgsVectorLayer* mLayer;
-    enum Mode { NoSelection, TextSelected, FeatureSelected } mMode;
-    QgsLabelPosition mCurrentLabel;
-    QgsRubberBand* mRubberBand;
-    QPointer<QgsSelectedFeature> mCurrentFeature;
-    int mCurrentVertex;
-    bool mIsRectangle;
-    QString mRectangleCRS;
-    QgsPoint mPressPos;
-    QgsPoint mPrevPos;
-
-    void clearCurrent( bool refresh = true );
-
-  private slots:
-    void updateLabelBoundingBox();
 };
 
 #endif // QGSREDLINING_H
