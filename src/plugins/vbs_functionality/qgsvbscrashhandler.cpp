ad9640d734ab20e3caf7239d67a168755f6ddd8b
Sandro Mani <manisandro@gmail.com>
2015-10-21 17:34:11 +0200

    [VBS Functionality] Change crash handler service URL
    Ref: 7358eb55db9cf2d9911bc2d4dfae0222186d6621

--- src/plugins/vbs_functionality/qgsvbscrashhandler.cpp
+++ src/plugins/vbs_functionality/qgsvbscrashhandler.cpp
@@ -26,7 +26,7 @@
 
 #ifdef _MSC_VER
 // Define the callback function that will be called on crash
-int CALLBACK CrashCallback(CR_CRASH_CALLBACK_INFO* pInfo)
+int CALLBACK CrashCallback( CR_CRASH_CALLBACK_INFO* pInfo )
 {
   // TODO: Attempt to save the project
   // QgsProject::instance()->
@@ -38,49 +38,49 @@ int CALLBACK CrashCallback(CR_CRASH_CALLBACK_INFO* pInfo)
 
 
 QgsVBSCrashHandler::QgsVBSCrashHandler()
-	: mHandlerInstalled(false)
+    : mHandlerInstalled( false )
 {
 #ifdef _MSC_VER
-	CR_INSTALL_INFO info;
-	memset(&info, 0, sizeof(CR_INSTALL_INFO)); 
-    info.cb = sizeof(CR_INSTALL_INFO);
-    info.pszAppName = _strdup(QString("QGIS %1").arg(QGis::QGIS_RELEASE_NAME).toLocal8Bit().data());
-    info.pszAppVersion = _strdup(QString("%1 (%2)").arg(QGis::QGIS_VERSION).arg(QGis::QGIS_DEV_VERSION).toLocal8Bit().data());
-	info.pszUrl = _strdup(QSettings().value("/vbsfunctionality/crashrpt_url", "http://cm004695.lt.admin.ch/MgdiServices/CrashReport.svc/Send").toString().toLocal8Bit().data());
-    info.dwFlags = 0;
-    info.dwFlags |= CR_INST_ALL_POSSIBLE_HANDLERS; // Install all available exception handlers.
-    info.dwFlags |= CR_INST_APP_RESTART; // Restart on crash
-	info.dwFlags |= CR_INST_AUTO_THREAD_HANDLERS; // Automatically install handlers to threads
-	info.dwFlags |= CR_INST_SHOW_ADDITIONAL_INFO_FIELDS;
-	info.dwFlags |= CR_INST_SEND_QUEUED_REPORTS;
-    info.uPriorities[CR_HTTP] = 1;
-    info.uPriorities[CR_SMTP] = CR_NEGATIVE_PRIORITY; // Disabled
-    info.uPriorities[CR_SMAPI] = CR_NEGATIVE_PRIORITY; // Disabled
- 
-    int nResult;
-	nResult = crInstall(&info);
- 
-    if(nResult!=0)
-	{
-        TCHAR buff[512];
-        crGetLastErrorMsg(buff, sizeof(buff));
-		QgsDebugMsg(QString("Failed to install crash reporter: %1").arg(QString::fromLocal8Bit(buff, sizeof(buff))));
-        return;
-    }
-	else
-	{
-		QgsDebugMsg("Crash reporter installed");
-		crSetCrashCallback(CrashCallback, 0);
-		mHandlerInstalled = true;
-	}
+  CR_INSTALL_INFO info;
+  memset( &info, 0, sizeof( CR_INSTALL_INFO ) );
+  info.cb = sizeof( CR_INSTALL_INFO );
+  info.pszAppName = _strdup( QString( "QGIS %1" ).arg( QGis::QGIS_RELEASE_NAME ).toLocal8Bit().data() );
+  info.pszAppVersion = _strdup( QString( "%1 (%2)" ).arg( QGis::QGIS_VERSION ).arg( QGis::QGIS_DEV_VERSION ).toLocal8Bit().data() );
+  info.pszUrl = _strdup( QSettings().value( "/vbsfunctionality/crashrpt_url", "http://npe.lt.admin.ch/MgdiServices/CrashReport.svc/Send" ).toString().toLocal8Bit().data() );
+  info.dwFlags = 0;
+  info.dwFlags |= CR_INST_ALL_POSSIBLE_HANDLERS; // Install all available exception handlers.
+  info.dwFlags |= CR_INST_APP_RESTART; // Restart on crash
+  info.dwFlags |= CR_INST_AUTO_THREAD_HANDLERS; // Automatically install handlers to threads
+  info.dwFlags |= CR_INST_SHOW_ADDITIONAL_INFO_FIELDS;
+  info.dwFlags |= CR_INST_SEND_QUEUED_REPORTS;
+  info.uPriorities[CR_HTTP] = 1;
+  info.uPriorities[CR_SMTP] = CR_NEGATIVE_PRIORITY; // Disabled
+  info.uPriorities[CR_SMAPI] = CR_NEGATIVE_PRIORITY; // Disabled
+
+  int nResult;
+  nResult = crInstall( &info );
+
+  if ( nResult != 0 )
+  {
+    TCHAR buff[512];
+    crGetLastErrorMsg( buff, sizeof( buff ) );
+    QgsDebugMsg( QString( "Failed to install crash reporter: %1" ).arg( QString::fromLocal8Bit( buff, sizeof( buff ) ) ) );
+    return;
+  }
+  else
+  {
+    QgsDebugMsg( "Crash reporter installed" );
+    crSetCrashCallback( CrashCallback, 0 );
+    mHandlerInstalled = true;
+  }
 #endif
 }
 
 QgsVBSCrashHandler::~QgsVBSCrashHandler()
 {
 #ifdef _MSC_VER
-	if(mHandlerInstalled)
-		crUninstall();
+  if ( mHandlerInstalled )
+    crUninstall();
 #endif
 }
 
