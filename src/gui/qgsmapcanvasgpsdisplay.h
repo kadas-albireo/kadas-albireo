edada581bce0ffbaa48d5166d271b73642699c1e
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2016-01-10 08:37:57 +0100

    Add license headers

--- src/gui/qgsmapcanvasgpsdisplay.h
+++ src/gui/qgsmapcanvasgpsdisplay.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                         qgsmapcanvasgpsdisplay.h
+                         ------------------------
+    begin                : Jan 2016
+    copyright            : (C) 2016 by Marco Hugentobler
+    email                : marco at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QGSMAPCANVASGPSDISPLAY_H
 #define QGSMAPCANVASGPSDISPLAY_H
 
