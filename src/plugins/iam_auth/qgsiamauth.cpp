0fd8e4b6584466ce7869ed95dabf0b701ba0069b
Sandro Mani <manisandro@gmail.com>
2016-03-29 13:49:56 +0200

    [iam] Initial demo implementation
    Ref: 01bb2cc6f257aec91d8c8820a7d0ada4d36d1503

--- src/plugins/iam_auth/qgsiamauth.cpp
+++ src/plugins/iam_auth/qgsiamauth.cpp
@@ -17,20 +17,136 @@
 
 #include "qgsiamauth.h"
 #include "qgisinterface.h"
+#include "qgsnetworkaccessmanager.h"
 #include "iamauth_plugin.h"
+#include "webaxwidget.h"
+
+#include <ActiveQt/QAxObject>
+#include <QDialog>
+#include <QMessageBox>
+#include <QNetworkCookieJar>
+#include <QSslConfiguration>
+#include <QStackedLayout>
+#include <QToolButton>
+#include <QUuid>
+#include <QWebView>
+#include <QWebPage>
+
+StackedDialog::StackedDialog(QWidget* parent, Qt::WindowFlags flags)
+  : QDialog(parent, flags)
+{
+  mLayout = new QStackedLayout();
+  setLayout(mLayout);
+}
+
+void StackedDialog::pushWidget(QWidget *widget)
+{
+  mLayout->setCurrentIndex(mLayout->addWidget(widget));
+}
+
+void StackedDialog::popWidget(QWidget *widget)
+{
+  if(mLayout->currentWidget() == widget) {
+    mLayout->setCurrentIndex( mLayout->currentIndex() - 1);
+  }
+  mLayout->removeWidget(widget);
+  widget->deleteLater();
+}
 
 QgsIAMAuth::QgsIAMAuth( QgisInterface * theQgisInterface )
     : QgisPlugin( sName, sDescription, sCategory, sPluginVersion, sPluginType )
-    , mQGisIface( theQgisInterface )
+    , mQGisIface( theQgisInterface ), mLoginButton(0), mLoginDialog(0)
 {
 }
 
 void QgsIAMAuth::initGui()
 {
-
+  mLoginButton = qobject_cast<QToolButton*>(mQGisIface->findObject("mLoginButton"));
+  if(mLoginButton) {
+    connect(mLoginButton, SIGNAL(clicked(bool)), this, SLOT(performLogin()));
+  }
 }
 
 void QgsIAMAuth::unload()
 {
+  disconnect(mLoginButton, SIGNAL(clicked(bool)), this, SLOT(performLogin()));
+  mLoginButton = 0;
+  delete mLoginDialog;
+  mLoginDialog = 0;
+}
 
+void QgsIAMAuth::performLogin()
+{
+  mLoginDialog = new StackedDialog(mQGisIface->mainWindow());
+  mLoginDialog->setWindowTitle(tr("eIAM Authentication"));
+  mLoginDialog->resize(640, 480);
+
+  WebAxWidget* webWidget = new WebAxWidget();
+  webWidget->setControl(QString::fromUtf8("{8856F961-340A-11D0-A96B-00C04FD705A2}"));
+  webWidget->dynamicCall("Navigate(const QString&)", QString("https://st74523a.lt.admin.ch/arcgis/home/signin.html"));
+  connect(webWidget, SIGNAL(NavigateComplete(QString)), this, SLOT(checkLoginComplete(QString)));
+  connect(webWidget, SIGNAL(NewWindow3(IDispatch**,bool&,uint,QString,QString)), this, SLOT(handleNewWindow(IDispatch**,bool&,uint,QString,QString)));
+  connect(webWidget, SIGNAL(WindowClosing(bool,bool&)), this, SLOT(handleWindowClose(bool,bool&)));
+  mLoginDialog->pushWidget(webWidget);
+  mLoginDialog->exec();
+}
+
+void QgsIAMAuth::checkLoginComplete(QString /*addr*/)
+{
+  if(mLoginDialog) {
+    WebAxWidget* webWidget = static_cast<WebAxWidget*>(QObject::sender());
+    QUrl url(webWidget->dynamicCall("LocationURL()").toString());
+	QUrl baseUrl;
+	baseUrl.setScheme(url.scheme());
+	baseUrl.setHost(url.host());
+    QAxObject* document = webWidget->querySubObject("Document()");
+    QStringList cookies = document->property("cookie").toString().split(QRegExp("\\s*;\\s*"));
+    foreach(const QString& cookie, cookies) {
+      QStringList pair = cookie.split("=");
+      if(!pair.isEmpty() && pair.first() == "esri_auth") {
+        QMessageBox::information(mLoginDialog, tr("Authentication successful"), QString("Url: %1\nCookie: %2").arg(baseUrl.toString()).arg(cookie));
+        mLoginDialog->accept();
+        mLoginDialog->deleteLater();
+        mLoginDialog = 0;
+        QNetworkCookieJar* jar = QgsNetworkAccessManager::instance()->cookieJar();
+        jar->setCookiesFromUrl(QList<QNetworkCookie>() << QNetworkCookie(cookie.toLocal8Bit()), baseUrl);
+
+        QWebView* view = new QWebView();
+        QWebPage* page = new QWebPage();
+        page->setNetworkAccessManager(QgsNetworkAccessManager::instance());
+        view->setPage(page);
+        QNetworkRequest req;
+        req.setUrl(QUrl("https://npe.lt.admin.ch/arcgis/sharing/rest/search"));
+        QSslConfiguration conf = req.sslConfiguration();
+        conf.setPeerVerifyMode(QSslSocket::VerifyNone);
+        req.setSslConfiguration(conf);
+        view->load(req);
+        view->show();
+        view->setAttribute(Qt::WA_DeleteOnClose);
+      }
+    }
+  }
+}
+
+void QgsIAMAuth::handleNewWindow(IDispatch** ppDisp, bool& cancel, uint /*dwFlags*/, QString /*bstrUrlContext*/, QString /*bstrUrl*/)
+{
+  if(mLoginDialog) {
+    WebAxWidget* webWidget = new WebAxWidget;
+    webWidget->setControl(QString::fromUtf8("{8856F961-340A-11D0-A96B-00C04FD705A2}"));
+    connect(webWidget, SIGNAL(NavigateComplete(QString)), this, SLOT(checkLoginComplete(QString)));
+    connect(webWidget, SIGNAL(NewWindow3(IDispatch**,bool&,uint,QString,QString)), this, SLOT(handleNewWindow(IDispatch**,bool&,uint,QString,QString)));
+    connect(webWidget, SIGNAL(WindowClosing(bool,bool&)), this, SLOT(handleWindowClose(bool,bool&)));
+    IDispatch* appDisp;
+    QAxObject* appDispObj = webWidget->querySubObject("Application()");
+    appDispObj->queryInterface(IID_IDispatch, (void**)&appDisp);
+    *ppDisp = appDisp;
+    mLoginDialog->pushWidget(webWidget);
+  }
+}
+
+void QgsIAMAuth::handleWindowClose(bool /*isChild*/, bool& /*cancel*/)
+{
+  if(mLoginDialog) {
+    mLoginDialog->popWidget(qobject_cast<QWidget*>(QObject::sender()));
+  }
 }
--- src/plugins/iam_auth/qgsiamauth.h
+++ src/plugins/iam_auth/qgsiamauth.h
@@ -19,7 +19,21 @@
 #define QGSIAMAUTH_H
 
 #include "qgisplugin.h"
+#include <QDialog>
 #include <QObject>
+
+class QStackedLayout;
+class QToolButton;
+struct IDispatch;
+
+class StackedDialog : public QDialog {
+public:
+  StackedDialog(QWidget* parent = 0, Qt::WindowFlags flags = 0);
+  void pushWidget(QWidget* widget);
+  void popWidget(QWidget *widget);
+private:
+  QStackedLayout* mLayout;
+};
 
 class QgsIAMAuth : public QObject, public QgisPlugin
 {
@@ -32,8 +46,14 @@ class QgsIAMAuth : public QObject, public QgisPlugin
 
   private:
     QgisInterface* mQGisIface;
+    QToolButton* mLoginButton;
+    StackedDialog* mLoginDialog;
 
   private slots:
+    void performLogin();
+    void checkLoginComplete(QString);
+    void handleNewWindow(IDispatch** ppDisp, bool& cancel, uint dwFlags, QString bstrUrlContext, QString bstrUrl);
+    void handleWindowClose(bool isChild, bool& cancel);
 };
 
 #endif // QGSIAMAUTH_H
