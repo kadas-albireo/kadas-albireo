687a3effe56d84b358b45839b0798eee84559e0f
Pirmin Kalberer <pka@sourcepole.ch>
2016-05-28 00:10:58 +0200

    Wording tweaks

--- src/core/geometry/qgswkbtypes.cpp
+++ src/core/geometry/qgswkbtypes.cpp
@@ -55,7 +55,7 @@ QgsWKBTypes::Type QgsWKBTypes::flatType( Type type )
 
 QgsWKBTypes::Type QgsWKBTypes::zmType( QgsWKBTypes::Type type, bool hasZ, bool hasM )
 {
-  type = flatType( type );
+  type = singleType( type );
   if ( hasZ )
     type = static_cast<QgsWKBTypes::Type>( static_cast<quint32>( type ) + 1000 );
   if ( hasM )
