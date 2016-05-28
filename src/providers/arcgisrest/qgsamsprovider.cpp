bdfca345382c8aa090cb09a5048cb8742aa58ab8
Sandro Mani <manisandro@gmail.com>
2016-05-28 09:16:12 +0200

    Fix format radioboxes in QgsSourceSelect not getting properly removed from layout

--- src/providers/arcgisrest/qgsamsprovider.cpp
+++ src/providers/arcgisrest/qgsamsprovider.cpp
@@ -80,7 +80,7 @@ void QgsAmsLegendFetcher::handleFinished()
     {
       QVariantMap legendEntryMap = legendEntry.toMap();
       QString label = legendEntryMap["label"].toString();
-      if ( label.isEmpty() && legendSymbols.size() == 1 )
+      if ( label.isEmpty() )
         label = queryResultMap["layerName"].toString();
       QByteArray imageData = QByteArray::fromBase64( legendEntryMap["imageData"].toByteArray() );
       legendEntries.append( qMakePair( label, QImage::fromData( imageData ) ) );
