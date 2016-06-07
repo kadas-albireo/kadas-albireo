eafc1338304fb57143c9b2dee496729c392387fc
Sandro Mani <manisandro@gmail.com>
2016-06-07 14:42:18 +0200

    [MilX] Rework upgrade/downgrade to handle entire milx file, not just mss strings

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -967,7 +967,7 @@ int main( int argc, char *argv[] )
   }
 
   // By default, assume offline. If online check succeeds below, set online
-  mySettings.setValue( "/qgis/isOffline", true );
+  mySettings.value( "/qgis/isOffline", true );
 
   //open default online/offline project if no project on command line and online test url is set
   QString templatePath;
@@ -992,7 +992,7 @@ int main( int argc, char *argv[] )
         {
           templatePath = QFileInfo( onlineProject ).isAbsolute() ? onlineProject : templateDirPath + "/" + onlineProject;
         }
-        mySettings.setValue( "/qgis/isOffline", false );
+        mySettings.value( "/qgis/isOffline", false );
       }
       else
       {
