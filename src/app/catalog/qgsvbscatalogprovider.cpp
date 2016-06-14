0e822623c9f2665c7c44e054c3ad92a877d39733
Sandro Mani <manisandro@gmail.com>
2016-06-14 13:36:00 +0200

    Expose QgsMapCanvas::wmtsResolutions

--- src/app/catalog/qgsvbscatalogprovider.cpp
+++ src/app/catalog/qgsvbscatalogprovider.cpp
@@ -38,10 +38,7 @@ QgsVBSCatalogProvider::QgsVBSCatalogProvider( const QString &baseUrl, QgsCatalog
 void QgsVBSCatalogProvider::load()
 {
   mPendingTasks = 0;
-  QUrl url( mBaseUrl );
-  QString lang = QSettings().value( "/locale/currentLang", "en" ).toString().left( 2 ).toUpper();
-  url.addQueryItem( "lang", lang );
-  QNetworkRequest req( url );
+  QNetworkRequest req( mBaseUrl );
   req.setRawHeader( "Referer", QSettings().value( "search/referer", "http://localhost" ).toByteArray() );
   QNetworkReply* reply = QgsNetworkAccessManager::instance()->get( req );
   connect( reply, SIGNAL( finished() ), this, SLOT( replyFinished() ) );
