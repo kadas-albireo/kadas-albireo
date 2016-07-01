fe973bd380e7cb285b3c07e096f74e7689743b48
Sandro Mani <manisandro@gmail.com>
2016-07-01 22:44:36 +0200

    [MilX] Update layer names in catalog when they are renamed
    Ref: 68cb661a1208641a863d7c734c9b3b6678b8f02d

--- src/plugins/milx/qgsmilxlibrary.cpp
+++ src/plugins/milx/qgsmilxlibrary.cpp
@@ -191,6 +191,7 @@ void QgsMilXLibrary::updateLayers()
   {
     if ( dynamic_cast<QgsMilXLayer*>( layer ) )
     {
+      connect( layer, SIGNAL( layerNameChanged() ), this, SLOT( updateLayers() ), Qt::UniqueConnection );
       mLayersCombo->addItem( layer->name(), layer->id() );
       if ( mIface->mapCanvas()->currentLayer() == layer )
       {
