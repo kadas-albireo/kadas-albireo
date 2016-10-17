a6f0a5c729500256b01a5e3b38a63ae9c0024937
Sandro Mani <manisandro@gmail.com>
2016-10-17 20:08:55 +0200

    Make analysis progress dialogs application modal, preventing the user from interacting with or even closing the main window
    Ref: b5ac5a3a0bc7fc37198f01ebd1f0ee650630c381

--- src/app/qgsmaptoolhillshade.cpp
+++ src/app/qgsmaptoolhillshade.cpp
@@ -108,7 +108,7 @@ void QgsMapToolHillshade::drawFinished()
   QgsHillshadeFilter hillshade( layer->source(), outputFile, "GTiff", spinHorAngle->value(), spinVerAngle->value(), rect, rectCrs );
   QProgressDialog p( tr( "Calculating hillshade..." ), tr( "Abort" ), 0, 0 );
   p.setWindowTitle( tr( "Hillshade" ) );
-  p.setWindowModality( Qt::WindowModal );
+  p.setWindowModality( Qt::ApplicationModal );
   QApplication::setOverrideCursor( Qt::WaitCursor );
   hillshade.processRaster( &p );
   QApplication::restoreOverrideCursor();
