006f8ece2c3395df60273fe27c7c769105378881
Sandro Mani <manisandro@gmail.com>
2017-10-10 13:35:05 +0200

    [OGR] List TIN and PolyhedralSurface as Polygon when searching sublayers
    Ref: 11d45d7bd8d8b9ddf7b9065a7770edc77dedc23d

--- src/providers/ogr/qgsogrprovider.cpp
+++ src/providers/ogr/qgsogrprovider.cpp
@@ -694,15 +694,15 @@ QStringList QgsOgrProvider::subLayers() const
         fCount[wkbPolygon] = fCount.value( wkbPolygon ) + fCount[wkbTriangle];
         fCount.remove( wkbTriangle );
       }
-      // List TIN and PolyhedralSurface as MultiPolygon
+      // List TIN and PolyhedralSurface as Polygon
       if ( fCount.contains( wkbTIN ) )
       {
-        fCount[wkbMultiPolygon] = fCount.value( wkbMultiPolygon ) + fCount[wkbTIN];
+        fCount[wkbPolygon] = fCount.value( wkbPolygon ) + fCount[wkbTIN];
         fCount.remove( wkbTIN );
       }
       if ( fCount.contains( wkbPolyhedralSurface ) )
       {
-        fCount[wkbMultiPolygon] = fCount.value( wkbMultiPolygon ) + fCount[wkbPolyhedralSurface];
+        fCount[wkbPolygon] = fCount.value( wkbPolygon ) + fCount[wkbPolyhedralSurface];
         fCount.remove( wkbPolyhedralSurface );
       }
 
